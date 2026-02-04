267358776e74bc939983ec990af189b5b5913ac5
Real-Lamafps <roaninfo25@gmail.com>
2026-02-04 14:24:33 +0100

    idk

--- Models/ToDoListModel.cs
+++ Models/ToDoListModel.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using Csharp_Fundament_Generics_Classes.Interfaces;
 
 namespace Csharp_Fundament_Generics_Classes.Models;
@@ -32,6 +33,32 @@ public class ToDoList
             if (todo != null)
             {
                 todos.Remove(todo);
+            }
+        }
+    }
+
+    public void Update()
+    {
+        var input = Console.ReadLine();
+
+        if (int.TryParse(input, out int id))
+        {
+            var update = todos.FirstOrDefault(t => t.Id == id);
+
+            if (update != null)
+            {
+                todos.Remove(update);
+
+                var title = Console.ReadLine();
+
+                todos.Add(
+                    new IToDoInfo
+                    {
+                        Id = Id++,
+                        Title = title ?? string.Empty,
+                        Done = false,
+                    }
+                );
             }
         }
     }
